52d79f7d9b0422b7fb88c35c7cdadd0a1c64d978
joengatia <ngatiajoe2@gmail.com>
2024-04-05 14:12:55 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// superbowlWin.js
+
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W" && game.team === "Denver Broncos");
+    return win ? win.year : undefined;
+}
+
+module.exports = superbowlWin;
+// superbowlWin.js
+
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W" && game.team === "Denver Broncos");
+    return win ? win.year : undefined;
+}
+
+module.exports = superbowlWin;
